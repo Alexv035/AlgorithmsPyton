fbbf9701cec3551e352df9962e42a12c24963f27
Alexander <59419330+Alexv035@users.noreply.github.com>
2020-05-13 11:24:29 +0300

    Lesson 1

--- Les_1_Task_3.py
+++ Les_1_Task_3.py
@@ -8,16 +8,16 @@ c. случайный символ.
 import random as rm
 
 # a. случайное целое число,
-x, y = input('Введите диапазон целых чисел в формате x,y: ').split(',')
-print(rm.randint(int(x), int(y)))
+x, y = input('Введите диапазон целых чисел в формате x,y: ').replace(' ','').split(',')
+print(rm.randint(int(x),int(y)))
 
 # b. случайное вещественное число
-x, y = input('Введите диапазон вещественных чисел в формате x,y: ').split(',')
-print(rm.uniform(float(x), float(y)))
+x, y = input('Введите диапазон вещественных чисел в формате x,y: ').replace(' ','').split(',')
+print(rm.uniform(float(x),float(y)))
 
 # c. случайный символ.
-x, y = input('Введите диапазон символов от a до z в формате x,y: ').split(',')
+x, y = input('Введите диапазон символов от a до z в формате x,y: ').replace(' ','').split(',')
 c = ord(x)
 d = ord(y)
-f = rm.randint(c, d)
+f = rm.randint(c,d)
 print(chr(f))
